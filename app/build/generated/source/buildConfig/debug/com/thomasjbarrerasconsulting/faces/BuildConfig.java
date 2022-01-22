22895c0d67f78bc33c84f1052ccd3fd224ceaf75
tombarreras <tombarreras@yahoo.com>
2022-01-22 07:34:13 -0800

    debug symbols

--- app/build/generated/source/buildConfig/debug/com/thomasjbarrerasconsulting/faces/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/thomasjbarrerasconsulting/faces/BuildConfig.java
@@ -7,8 +7,8 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.thomasjbarrerasconsulting.faces";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 14;
-  public static final String VERSION_NAME = "1.14";
+  public static final int VERSION_CODE = 16;
+  public static final String VERSION_NAME = "1.16";
   // Field from default config.
   public static final String BASE64_ENCODED_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzy1ZF7P9DqnBBbSbEbpwkEFYzx8mT586z1Eh4Jtl+qpCMhTrCCcK9b6o2HJUm0cb7/ITOb74vJc/Gt4cUnbpkaJc4WUDnl8DhVLyDjnnUoJKRw8L2tUGlpZFhoz6JZxAWx6ISYU1eJyHuVZV5JFae2R1budaU2SDvG+HuQywik34vi5ZemFaKIyku88mr2ZgeB4yAAJmJGIK6IUTTEDJ7WodKL3MvEHKcAGAFFgHLITvVWDOeZPezdjkMoWvxsUnXY5w1l7VFgHhmHQq/pClHxX3O6J7mE75/XEiXzBiYH3anJkMRiREJdm+Mn8wFzaERHycp3NeIwHzFHmRFmpskQIDAQAB";
 }
